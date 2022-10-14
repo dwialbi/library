252e66d678a6b555dd19f5111147101509d8afde
RGS <nuradlifathan@gmail.com>
2022-10-14 21:48:42 +0700

    authMiddleware

---
+++ library-backend/middlewares/authMiddleware.js
@@ -0,0 +1,32 @@
+const { validToken } = require("../lib/jwt")
+
+const verifyToken = (req, res, next) => {
+  let token = req.headeares.authorization
+
+  if (!token) {
+    return res.status(401).json({
+      message: "User is unauthorized",
+    })
+  }
+
+  try {
+    token = token.split(" ")[1]
+
+    const userVerify = validToken(token)
+
+    if (!userVerify) {
+      return res.status(401).json({
+        message: "unauthorized request",
+      })
+    }
+
+    req.user = userVerify
+
+    next()
+  } catch (err) {
+    console.log(err)
+    return res.status(401).json({
+      message: "Token Error!",
+    })
+  }
+}
